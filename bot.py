d62142f99fd7de0b0bb0570187b454d779f8fefe
jwadhassank-tech <jwad.hassan.k@gmail.com>
2026-04-15 17:29:58 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,10 @@ from telegram.ext import (
 )
 
 # ---------------- CONFIG ----------------
+import os
+print("BOT TOKEN =", os.getenv("BOT_TOKEN"))
 BOT_TOKEN = os.getenv("8700904545:AAEPG5zIwwlIPSMklc4QtIKbzJ_-VAizjB4")
-ADMIN_GROUP_ID = -1003967160997  # حط ID القروب هنا
-
+ADMIN_GROUP_ID = -1003967160997
 # ---------------- DATABASE ----------------
 conn = sqlite3.connect("tickets.db", check_same_thread=False)
 cursor = conn.cursor()
